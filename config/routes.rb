3b65b8bcb8a7c143cb990728dca152e4641e4401
krandles <krandles@gmail.com>
2018-03-14 14:17:32 -0400

    working on auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :invitations
   resources :posts
   resources :users
+
+  post "/login", to: 'auth#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
