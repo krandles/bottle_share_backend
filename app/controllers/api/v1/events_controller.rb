c2529a1045d8a7487db24a277e8b14e74c911e40
krandles <krandles@gmail.com>
2018-05-07 19:04:19 -0400

    removed location from events controller create method

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::EventsController < ApplicationController
     @event = Event.new(event_params)
 
     if @event.save
-      render json: @event, status: :created, location: @event
+      render json: @event, status: :created
     else
       render json: @event.errors, status: :unprocessable_entity
     end
