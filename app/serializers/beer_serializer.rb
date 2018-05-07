16eb29981a00452e684301ffc3ea78942902d772
krandles <krandles@gmail.com>
2018-05-07 19:45:30 -0400

    add brewery details to beer serializer

--- app/serializers/beer_serializer.rb
+++ app/serializers/beer_serializer.rb
@@ -1,3 +1,4 @@
 class BeerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :abv, :brewery, :style, :img_url, :description, :brewery
+  attributes :id, :name, :abv, :brewery, :style, :img_url, :description
+  has_one :brewery
 end
